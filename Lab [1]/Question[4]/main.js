5dd73ca807607bbeb2791a0968aac7d1b4796553
omarkhalil117 <omarkhalil117@gmail.com>
2023-12-16 10:40:08 +0200

    Adding index.html to labs

--- Lab [1]/Question[4]/main.js
+++ Lab [1]/Question[4]/main.js
@@ -4,4 +4,9 @@ var num2 = prompt("Enter Second number")
 
 var res = Number(num1) + Number(num2)
 
+if(isNaN(res))
+{
+    alert("Wrong input")
+}
+
 alert(`Result of sum ${num1} and ${num2} = ${res}`)
